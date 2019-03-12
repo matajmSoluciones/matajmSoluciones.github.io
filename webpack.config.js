5914d53a73e0959f8c018029b3c7909cd0f7090b
Jhonny Mata <solucionesmatajm@gmail.com>
2019-03-12 03:44:59 -0400

    agregando loaders para vue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path"),
     ExtractTextPlugin = require('extract-text-webpack-plugin'),
     extractCSS = new ExtractTextPlugin('style.css'),
+    VueLoaderPlugin = require('vue-loader/lib/plugin'),
     Dotenv = require('dotenv-webpack');
 
 
@@ -16,12 +17,21 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].js"
     },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.js'
+        },
+    },
     module: {
         rules: [
             {
                 test: /\.(woff2?|eot|ttf|otf|svg|jpeg|jpg|png|gif)(\?.*)?$/,
                 loader: 'url-loader',
                 options: { limit: 1000, name: 'fonts/[name].[ext]', },
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
             },
             {
                 test: /\.(sass|scss)$/,
@@ -39,6 +49,7 @@ module.exports = {
     },
     plugins: [
         extractCSS,
-        new Dotenv()
+        new Dotenv(),
+        new VueLoaderPlugin()
     ]
 };
