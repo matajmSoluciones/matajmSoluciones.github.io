53e355373f4600e48994c5ce8b71fb17ea9fb88f
Jhonny Mata <solucionesmatajm@gmail.com>
2019-04-06 10:11:41 -0400

    agregado script activar y desactivar modal

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,28 @@ window.addEventListener("load", function () {
             mailto();
         });
     }
+    var buttonsshow = document.querySelectorAll("*[data-show]");
+    for (var i = 0, n = buttonsshow.length; i < n; i++) {
+        var button = buttonsshow[i];
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            var element = document.getElementById(this.getAttribute("data-show"));
+            if (!element) {
+                return;
+            }
+            element.classList.add("is-active");
+        });
+    }
+    var buttonclose = document.querySelectorAll(".modal .modal-close, .modal .modal-background");
+    for (var i = 0, n = buttonclose.length; i < n; i++) {
+        var button = buttonclose[i];
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            var element = event.target.parentNode;
+            if (!element) {
+                return;
+            }
+            element.classList.remove("is-active");
+        });
+    }
 });
